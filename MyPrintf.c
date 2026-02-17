b6d8276412525bd1e8fd44dfe4f0a61bda64db1c
luca01github <luca.porcheddu01@gmail.com>
2026-02-17 23:28:31 +0100

    Refactor my_printf and related functions

--- MyPrintf.c
+++ MyPrintf.c
@@ -1,12 +1,11 @@
-#include <stdarg.h>
-#include <unistd.h>
+#include <stdarg.h>  //variadic arguments
+#include <unistd.h>  //system calls
 
+//write(1=STDOUT, &c = pointer to the value to print, 1 = number of bytes (1 char))
 void my_putc(char c) { write(1, &c, 1); }
 
 void my_putstr(const char *str) {
-    if (!*str) {
-        return;
-    }
+    if (!str) str = "(null)";
     while (*str) {
         my_putc(*str);
         str++;
@@ -14,33 +13,35 @@ void my_putstr(const char *str) {
 }
 
 void my_putint(int num) {
-    long n = num;
+    long n = num;  //long to ensure there is no MIN_INT overflow
 
     if (n < 0) {
         my_putc('-');
-        n *= -1;
+        n *= -1;  //converting to positive
     }
 
     if (n >= 10) {
-        my_putint(n / 10);
+        my_putint(n / 10);  //recursive step to print digits in right order
     }
-    my_putc((n % 10) + '0');
+    my_putc((n % 10) + '0');  //getting each digit, and also converting to char
 }
 
 void my_putf(float n) {
-    int precision = 6;
+    int precision = 6;  //number of digits after '.'
 
     if (n < 0) {
         my_putc('-');
         n *= -1;
     }
 
+    //separating before and after comma
     int intpart = (int)n;
     float floatpart = n - intpart;
 
-    my_putint(intpart);
+    my_putint(intpart);  
     my_putc('.');
 
+    //loop to print each digit after comma
     for (int i = 0; i < precision; i++) {
         floatpart *= 10;
         my_putint((int)floatpart);
@@ -49,12 +50,12 @@ void my_putf(float n) {
 }
 
 void my_printf(const char *format, ...) {
-    va_list ap;
-    va_start(ap, format);
+    va_list ap;  //object to hold the information needed to retrieve additional arguments
+    va_start(ap, format);  //initializes 'ap' to point to the first argument after 'format'
 
     while (*format) {
         if (*format == '%') {
-            format++;
+            format++;  //looking at the character after '%'
 
             if (!*format) {
                 break;
@@ -62,7 +63,7 @@ void my_printf(const char *format, ...) {
 
             switch (*format) {
             case 'c': {
-                char c = va_arg(ap, int);
+                char c = va_arg(ap, int);  //fetch an int from arguments
                 my_putc(c);
                 break;
             }
@@ -82,10 +83,12 @@ void my_printf(const char *format, ...) {
                 break;
             }
             case '%': {
+                // %% should print %
                 my_putc('%');
                 break;
             }
             default: {
+                //for example: %j should print exactly %j, not just j
                 my_putc('%');
                 my_putc(*format);
                 break;
@@ -99,7 +102,7 @@ void my_printf(const char *format, ...) {
         format++;
     }
 
-    va_end(ap);
+    va_end(ap);  //cleans up the variadic argument list before the function returns
 }
 
 int main() {
